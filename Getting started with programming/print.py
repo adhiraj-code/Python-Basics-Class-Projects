d2bb3aba63e8a1a835d7c32480cbcecdc284e9e1
adhiraj-code <alkarathore87@gmail.com>
2025-12-21 13:48:49 +0530

    Add files via upload

---
+++ Getting started with programming/print.py
@@ -0,0 +1,7 @@
+a = 10
+
+print(a)
+
+print("The value of a is : ",a)
+
+print("This is my first code",end= ".")
---
+++ Getting started with programming/variables.py
@@ -0,0 +1,15 @@
+# Rules to declare a varible name
+
+# 1. variable name should start with letter
+
+# 2. variable name should not start with number or special characters
+
+# 3. varibale name should not have spaces
+
+# 4. variable name can be followed by numbers or special characters
+
+name1= "Adhiraj"
+
+print(name1)
+
+a = int(input('Enter a number'))
