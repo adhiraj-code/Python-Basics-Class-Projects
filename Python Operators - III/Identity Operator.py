8b55b48fffb08e798df87c4941b4564a4b35cb32
adhiraj-code <alkarathore87@gmail.com>
2026-01-17 11:44:20 +0530

    Add files via upload

---
+++ Python Operators - III/Identity Operator.py
@@ -0,0 +1,23 @@
+x = 7
+
+if type(x) is int:
+    print("True")
+else:
+    print("False")
+
+x = 59
+y = 45
+
+if x is not y:
+    print("x and y are not identical")
+else:
+    print("x and y are identical")
+
+x = 59
+y = 59
+
+if x is y:
+    print("x and y are identical")
+else:
+    print("x and y are not identical")
+    
